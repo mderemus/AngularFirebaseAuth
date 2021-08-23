5d2646e7d61546b59542b3f30b6ebdc4d714aa91
mderemus <mike.deremus@gmail.com>
2021-08-22 21:11:51 -0500

    Intial Check-in

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDN7OWBrWzLvdYgEPPurD7QcO7RWQ7bhYw",
+    authDomain: "talkalotta-b9999.firebaseapp.com",
+    projectId: "talkalotta-b9999",
+    storageBucket: "talkalotta-b9999.appspot.com",
+    messagingSenderId: "122315458622",
+    appId: "1:122315458622:web:b988d0349dcb057b6be96f",
+    measurementId: "G-3MQZF7HE3M"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDN7OWBrWzLvdYgEPPurD7QcO7RWQ7bhYw",
+    authDomain: "talkalotta-b9999.firebaseapp.com",
+    projectId: "talkalotta-b9999",
+    storageBucket: "talkalotta-b9999.appspot.com",
+    messagingSenderId: "122315458622",
+    appId: "1:122315458622:web:b988d0349dcb057b6be96f",
+    measurementId: "G-3MQZF7HE3M"
+  }
 };
 
 /*
